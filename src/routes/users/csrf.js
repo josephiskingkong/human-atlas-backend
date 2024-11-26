cdf65b607bcc66f06fda87917baafe5e1eb2305e
Egor <117137864+josephiskingkong@users.noreply.github.com>
2024-11-27 01:36:34 +0700

    fix: csrf

--- src/routes/users/csrf.js
+++ src/routes/users/csrf.js
@@ -1,6 +1,11 @@
 const app = require("../../config/express");
 
 app.get('/v1/csrf-token', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken(), { httpOnly: true, maxAge: 3600000 });
+    res.cookie('XSRF-TOKEN', req.csrfToken(), {
+        httpOnly: true,
+        maxAge: 3600000, 
+        sameSite: 'None',
+        secure: true, 
+    });
     res.json({ csrfToken: req.csrfToken() });
 });
