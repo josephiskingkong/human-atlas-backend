f2edf4e1f56700e66b6d6f29b32c1eb74131d316
Egor <117137864+josephiskingkong@users.noreply.github.com>
2024-12-07 19:02:02 +0700

    fix cookies

--- src/config/express.js
+++ src/config/express.js
@@ -23,9 +23,8 @@ app.use(cookieParser());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cookieParser());
 
-const csrfProtection = csrf({});
+const csrfProtection = csrf();
 
 app.use(csrfProtection);
 
