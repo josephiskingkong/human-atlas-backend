259f305d8f4ec0df48d41203f271022ffb4e486a
Egor <117137864+josephiskingkong@users.noreply.github.com>
2024-12-07 19:13:16 +0700

    fix cookies

--- src/config/express.js
+++ src/config/express.js
@@ -11,7 +11,7 @@ app.use(cors({
     origin: ['http://localhost:3000', 'https://josephiskingkong.github.io'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-XSRF-TOKEN', 'XSRF-TOKEN'],
+    allowedHeaders: '*',
 }));
 
 app.use((req, res, next) => {
