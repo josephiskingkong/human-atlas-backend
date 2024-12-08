0571f31b168bb35e954d45ba784f640951b31c29
Egor <117137864+josephiskingkong@users.noreply.github.com>
2024-12-08 18:22:40 +0700

    fix auth

--- src/config/express.js
+++ src/config/express.js
@@ -12,6 +12,7 @@ app.use(cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
+        'Cookie',
         'Content-Type',
         'Authorization',
         'X-XSRF-TOKEN',
