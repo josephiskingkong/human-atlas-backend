c1d96e1ae2363dca2136d05ce24fde231ea81aee
Egor <117137864+josephiskingkong@users.noreply.github.com>
2025-03-22 22:01:20 +0700

    fix: limits

--- src/config/express.js
+++ src/config/express.js
@@ -32,8 +32,8 @@ app.use((req, res, next) => {
 
 app.use(cookieParser());
 
-app.use(bodyParser.json({ limit: '2048mb' }));
-app.use(bodyParser.urlencoded({ limit: '2048mb', extended: true }));
+app.use(bodyParser.json({ limit: '1024mb' }));
+app.use(bodyParser.urlencoded({ limit: '1024mb', extended: true }));
 app.use(cookieParser());
 
 // const csrfProtection = csrf({
