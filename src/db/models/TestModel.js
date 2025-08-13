62240e7a74adbaa5e84774ef274ce28fbede3710
agartanah <vyatin2017@mail.ru>
2025-08-14 00:54:56 +0700

    fix: remove category id in test model

--- src/db/models/TestModel.js
+++ src/db/models/TestModel.js
@@ -1,36 +1,40 @@
-const { DataTypes } = require('sequelize');
-const { db } = require('../db');
+const { DataTypes } = require("sequelize");
+const { db } = require("../db");
 
-const TestModel = db.define('tests', {
+const TestModel = db.define(
+  "tests",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        unique: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true,
     },
     title: {
-        type: DataTypes.STRING(255),
-        allowNull: false
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
-    categoryId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
+    // categoryId: {
+    //     type: DataTypes.INTEGER,
+    //     allowNull: false
+    // },
     duration: {
-        type: DataTypes.INTEGER, // длительность в минутах
-        allowNull: false
+      type: DataTypes.INTEGER, // длительность в минутах
+      allowNull: false,
     },
     questionCount: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
     },
     description: {
-        type: DataTypes.TEXT,
-        allowNull: true
-    }
-}, {
-    timestamps: true // включаем createdAt и updatedAt
-});
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: true, // включаем createdAt и updatedAt
+  }
+);
 
 module.exports = { TestModel };
