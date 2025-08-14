77878bff0f68417405438f68fec6ff4830d13109
agartanah <vyatin2017@mail.ru>
2025-08-14 22:11:15 +0700

    fix: deleting tiles when cascade deleting

--- src/db/models/CategoryModel.js
+++ src/db/models/CategoryModel.js
@@ -1,16 +1,47 @@
-const { DataTypes } = require('sequelize')
-const { db } = require('../db')
+const { DataTypes } = require("sequelize");
+const { db } = require("../db");
+const fs = require("fs");
+const { logger } = require("../../config/logger");
+const { OrganModel } = require("./OrganModel");
 
-const CategoryModel = db.define('categories', {
+const CategoryModel = db.define(
+  "categories",
+  {
     id: {
-        type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true,
     },
     name: {
-        type: DataTypes.STRING, allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     categoryId: {
-        type: DataTypes.INTEGER, allowNull: true
-    }
-})
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    hooks: {
+      async beforeDestroy(category, options) {
+        const organs = await OrganModel.findAll({
+          where: { categoryid: category.id },
+        });
 
-module.exports = { CategoryModel }
+        for (const organ of organs) {
+          const organPath = `/var/www/human-atlas-tiles/tiles/${organ.id}`;
+
+          try {
+            await fs.promises.rm(organPath, { recursive: true, force: true });
+            logger.info(`Папка удалена: ${organPath}`);
+          } catch (err) {
+            logger.error(`Ошибка удаления папки: ${err.message}`);
+          }
+        }
+      },
+    },
+  }
+);
+
+module.exports = { CategoryModel };
--- src/db/models/OrganModel.js
+++ src/db/models/OrganModel.js
@@ -1,34 +1,61 @@
-const { DataTypes } = require('sequelize')
-const { db } = require('../db')
+const { DataTypes } = require("sequelize");
+const { db } = require("../db");
+const fs = require("fs");
+const { logger } = require("../../config/logger");
 
-const OrganModel = db.define('organs', {
+const OrganModel = db.define(
+  "organs",
+  {
     id: {
-        type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true,
     },
     name: {
-        type: DataTypes.TEXT, allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
     synonym: {
-        type: DataTypes.TEXT, allowNull: false 
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
     categoryid: {
-        type: DataTypes.INTEGER, allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     status: {
-        type: DataTypes.STRING, allowNull: false, defaultValue: "PROCESSING"
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "PROCESSING",
     },
     mpp_x: {
-        type: DataTypes.FLOAT
+      type: DataTypes.FLOAT,
     },
     mpp_y: {
-        type: DataTypes.FLOAT
+      type: DataTypes.FLOAT,
     },
-    width: { 
-        type: DataTypes.BIGINT
+    width: {
+      type: DataTypes.BIGINT,
     },
     height: {
-        type: DataTypes.BIGINT
-    }
-})
+      type: DataTypes.BIGINT,
+    },
+  },
+  {
+    hooks: {
+      async beforeDestroy(organ) {
+        const organPath = `/var/www/human-atlas-tiles/tiles/${organ.id}`;
 
-module.exports = { OrganModel }
+        try {
+          await fs.promises.rm(organPath, { recursive: true, force: true });
+          logger.info(`Папка удалена: ${organPath}`);
+        } catch (err) {
+          logger.error(`Ошибка удаления папки: ${err.message}`);
+        }
+      },
+    },
+  }
+);
+
+module.exports = { OrganModel };
